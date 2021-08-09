38814c302ec4ffa6982d49acc983405f418c55e6
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-08 22:50:48 -0400

    create function to create new item

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('new-item-form'); 
 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let name = e.target[0].value;
+  })
 })
+
+function CreateNewItem(name) {
+  return fetch('localhost:3000/')
+    .then(req => req.json())
+    .then(response => console.log(response))
+}
