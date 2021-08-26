036434804fcf2c35b742b0fe8a6ae8c51377d93b
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-26 15:47:08 -0400

    changed the password

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const indexRouter = require('./routes/index');
 const db = mySql.createConnection({
   host:'127.0.0.1',
   user: 'root',
-  password: 'Test1234'
+  password: 'testtest'
 });
 
 // Connect 
