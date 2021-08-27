c99f19d67a5467f33fa6470b466cee646c770389
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 13:49:48 -0400

    removed comments

--- backend/app.js
+++ backend/app.js
@@ -19,24 +19,6 @@ const sync = async () => await sequelize.sync({alter:true})
 sync()
 
 
-// Create connection
-// const db = mySql.createConnection({
-//   host:'localhost',
-//   user: 'root',
-//   password: 'testtest'
-// });
-
-// // Connect 
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('mySQL connected')
-// })
-
-
-
-
 const app = express();
 
 app.use(logger('dev'));
@@ -52,31 +34,6 @@ app.use(upload.array())
 
 app.use('/', indexRouter);
 
-// app.get('/createDB', (req, res) => {
-//   console.log('running')
-//   let sql = "CREATE DATABASE nodemysql";
-//   db.query(sql, (err, result) => {
-//     if(err) {throw err}
-//     console.log(result)
-//     res.send('Database Created...')
-//   })
-// })
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 app.listen(3000, () => {
   console.log('server started on port 3000')
