9cc0f91b0e0070cf6c843f735e259dcb701ec638
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-01 14:53:02 -0400

    created new function to create item

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,17 +5,21 @@ import { useState } from 'react';
 function App() {
   const [items, setItems] = useState([])
 
+  const createItem = e => {
+    e.preventDefault();
+    const newITem = {
+      name: e
+    }
+    console.log(newITem)
+  }
+
   return (
-    
-    
     <div id="header">
       <h1>Todo List!</h1>
-      <form id="new-item-form">
+      <form id="new-item-form" onSubmit={createItem}>
         <input id="form-input" type="text" name="name" placeholder="name" />
       </form> 
     </div>
-    
-  
   );
 }
 
