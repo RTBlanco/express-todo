4158eecb8bfe927b96aed2c28604e201cfa515a6
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-01 15:47:31 -0400

    made it possible to switch from edit to normal

--- react-frontend/src/Item.js
+++ react-frontend/src/Item.js
@@ -1,13 +1,40 @@
+import pencil from '../src/images/pencil.png';
+import xBtn from '../src/images/X.png';
+import {useState} from 'react';
+
 
 function Item({name, id}){
+  const [editing, setEditing] = useState(false);
+
+
+
+  const renderNormal = () => {
+    return (
+      <>
+        <h3>{name}</h3>
+        <div class="settings">
+          <button className="edit" ><img src={pencil} alt="x"/></button>
+          <button className="remove" ><img src={xBtn} alt="pencil"/></button>
+        </div>
+      </>
+    )
+  }
+
+  const renderEdit = () => {
+    return (
+      <>
+        <input className="edit-input" type="text" name="name" value="${this.name}" />
+        <div className="settings">
+          <button class="remove"><img src={xBtn} alt="x"/></button>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="item">
-        <h3>{name}</h3>
-        <div class="settings">
-          <button class="edit" ><img src="src/images/pencil.png" alt="x"/></button>
-          <button class="remove" ><img src="src/images/X.png" alt="pencil"/></button>
-        </div>
+        {editing ? renderEdit() : renderNormal()}
       </div>
     </>
   )
