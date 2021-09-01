ba5c156b122acaf980d4662688e2d4be55e0218b
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-01 16:31:37 -0400

    added new fetch functions

--- react-frontend/src/helper/connection.js
+++ react-frontend/src/helper/connection.js
@@ -1,1 +1,55 @@
+const BASE_URL = "http://localhost:3000";
 
+export function fetchItems() {
+  return fetch(BASE_URL)
+    .then((req) => req.json())
+    .then((response) => {
+      for (let i of response) {
+        let item = new Item(i);
+        item.render();
+      }
+    });
+}
+
+
+export function createNewItem(item) {
+  return fetch(`${BASE_URL}/new`, {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: item,
+  })
+    .then((req) => req.json())
+    .then((response) => {
+      console.log(response);
+      let newItem = new Item(response);
+      newItem.render();
+    });
+}
+
+export function deleteItem(item) {
+  return fetch(`${BASE_URL}/${item.id}`, {
+    method: "DELETE",
+    header: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then((item = console.log(item)))
+    .catch((error) => console.error(error));
+}
+
+export function updateItem(item, formData) {
+  return fetch(`${BASE_URL}/${item.id}`, {
+    method: "PATCH",
+    header: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: formData
+  })
+    .then((req) => req.json())
+    .then((item) => console.log(item));
+}
