93d13e8b6ffaf6d053cda61537f69fa25dd1448f
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-07 19:30:41 -0400

    added the needed components

--- iosApp/App.js
+++ iosApp/App.js
@@ -15,14 +15,34 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput
 } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Todo app</Text>
+    <View style={styles.header}>
+      <Text style={styles.h1}>Todo List!</Text>
+      <TextInput placeholder="name"/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+
+  },
+
+  h1: {
+
+    // display: block;
+    // fontSize: 2em;
+    // margin-top: 0.67em;
+
+    // margin-bottom: 0.67em;
+    // margin-left: 0;
+    // margin-right: 0;
+    // font-weight: bold;
+  }
+})
 
 export default App;
