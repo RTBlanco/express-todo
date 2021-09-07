989341bd0057db9379dc1cb456a91e9fd79d8012
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-07 19:59:44 -0400

    create container to holde the items when they are created

---
+++ iosApp/components/Items.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput
+} from 'react-native';
+
+const Items = () => {
+  return (
+    <View style={styles.items}>
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  items: {
+    flex: 2,
+    backgroundColor: '#4CC9F0'
+  }
+})
+
+export default Items;
