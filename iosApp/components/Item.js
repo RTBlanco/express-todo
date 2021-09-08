98596f5b0a5946849f5414295af0a43057795b03
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-08 18:05:04 -0400

    moved the name to more in the middle

--- iosApp/components/Item.js
+++ iosApp/components/Item.js
@@ -13,8 +13,8 @@ import {
 
 const Item = () => {
   return (
-    <View>
-      <Text style={styles.h3}>Name</Text>
+    <View style={styles.item}>
+      <Text style={styles.h3}>Test</Text>
       <View style={styles.settings}>
         {/* buttons */}
       </View>
@@ -24,7 +24,20 @@ const Item = () => {
 
 const styles = StyleSheet.create({
   h3: {
+    // flex: 1,
+    fontSize: 20,
+    marginTop: 1,
+    marginBottom: 1,
+    fontWeight: 'bold'
+  },
 
+  item: {
+    // flex: 1,
+    padding: 10,
+    // marginTop: 10,
+    // marginBottom: 10,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 3
   },
 
   settings: {
