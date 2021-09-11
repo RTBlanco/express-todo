5e162bb8c38eaf5f5e7a2dfe922fcb506bc4d067
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-11 07:38:29 -0400

    Removed unused functions

--- iosApp/components/Items.js
+++ iosApp/components/Items.js
@@ -6,9 +6,6 @@ import {
 import Item from './Item';
 
 const Items = ({items, remove}) => {
-  const renderItems = () => {
-    return items.map(item => <Item key={item.id} item={item} remove={remove} />)
-  }
   return (
     <FlatList style={styles.items} data={items} renderItem={({item}) => <Item key={item.id} item={item} remove={remove} />}/>
   )
