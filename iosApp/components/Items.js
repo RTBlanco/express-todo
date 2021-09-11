6f5b11d082677e05eff5c0ead0bb0399ab62e61f
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-11 07:37:06 -0400

    Removed comments

--- iosApp/components/Items.js
+++ iosApp/components/Items.js
@@ -17,11 +17,6 @@ const Items = ({items, remove}) => {
     return items.map(item => <Item key={item.id} item={item} remove={remove} />)
   }
   return (
-    // <View style={styles.items}>
-    //   {/* Render the items */}
-    //   {/* <Item /> */}
-    //   {renderItems()}
-    // </View>
     <FlatList style={styles.items} data={items} renderItem={({item}) => <Item key={item.id} item={item} remove={remove} />}/>
   )
 }
